dfa444800fc37ed651faea24bf61cead8961587a
jdegrandchamp89 <degranjo@mail.gvsu.edu>
2017-05-29 20:42:01 -0400

    new button plus home page activity

--- app/src/main/java/com/example/john/socialgolf/LoginActivity.java
+++ app/src/main/java/com/example/john/socialgolf/LoginActivity.java
@@ -3,6 +3,7 @@ package com.example.john.socialgolf;
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.support.annotation.NonNull;
 import android.support.design.widget.Snackbar;
@@ -49,7 +50,7 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
      * TODO: remove after connecting to a real authentication system.
      */
     private static final String[] DUMMY_CREDENTIALS = new String[]{
-            "foo@example.com:hello", "bar@example.com:world"
+            "foo@example.com:hello", "bar@example.com:world", "jdegrandchamp89@gmail.com:jojobe123"
     };
     /**
      * Keep track of the login task to ensure we can cancel it if requested.
@@ -333,6 +334,8 @@ public class LoginActivity extends AppCompatActivity implements LoaderCallbacks<
             showProgress(false);
 
             if (success) {
+                Intent toHome = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(toHome);
                 finish();
             } else {
                 mPasswordView.setError(getString(R.string.error_incorrect_password));
