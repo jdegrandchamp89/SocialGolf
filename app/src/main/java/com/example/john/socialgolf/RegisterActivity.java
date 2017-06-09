468f8ad235c88d4ad272542aec831f53d0fdc62f
jdegrandchamp89 <degranjo@mail.gvsu.edu>
2017-06-09 13:32:15 -0400

    add registration code

--- app/src/main/java/com/example/john/socialgolf/RegisterActivity.java
+++ app/src/main/java/com/example/john/socialgolf/RegisterActivity.java
@@ -1,15 +1,39 @@
 package com.example.john.socialgolf;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.annotation.TargetApi;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
 
 public class RegisterActivity extends AppCompatActivity {
+
+    private EditText mEmail;
+    private EditText mPassword;
+    private EditText mPasswordConfirm;
+    private EditText mName;
+    private View mProgressView;
+    private FirebaseAuth mAuth;
+    private static final String TAG = "RegisterUser";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,6 +41,12 @@ public class RegisterActivity extends AppCompatActivity {
         setContentView(R.layout.activity_register);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        mEmail = (EditText) findViewById(R.id.email);
+        mPassword = (EditText) findViewById(R.id.password);
+        mPasswordConfirm = (EditText) findViewById(R.id.passwordConfirm);
+        mName = (EditText) findViewById(R.id.name);
+        mProgressView = findViewById(R.id.register_progress);
 
 //        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 //        fab.setOnClickListener(new View.OnClickListener() {
@@ -30,10 +60,169 @@ public class RegisterActivity extends AppCompatActivity {
 
         Button completeRegistration = (Button) findViewById(R.id.Complete_Registration);
         completeRegistration.setOnClickListener(v -> {
-            Intent toHome = new Intent(RegisterActivity.this, NavDrawerActivity.class);
-            startActivity(toHome);
-            finish();
+            attemptRegistration();
         });
+
+        mAuth = FirebaseAuth.getInstance();
+    }
+
+    private void attemptRegistration(){
+
+        // Reset errors.
+        mEmail.setError(null);
+        mPassword.setError(null);
+        mPasswordConfirm.setError(null);
+        mName.setError(null);
+
+        // Store values at the time of the login attempt.
+        String email = mEmail.getText().toString();
+        String password = mPassword.getText().toString();
+        String passwordConfirm = mPasswordConfirm.getText().toString();
+        String name = mName.getText().toString();
+
+        boolean cancel = false;
+        View focusView = null;
+
+        // Check for a valid password, if the user entered one.
+        if (!TextUtils.isEmpty(password) && !isPasswordValid(password)
+                && !TextUtils.isEmpty(passwordConfirm) && !isPasswordValid(passwordConfirm)
+                && !password.contentEquals(passwordConfirm)) {
+            mPassword.setError(getString(R.string.error_invalid_password));
+            focusView = mPassword;
+            cancel = true;
+        }
+
+        // Check for a valid email address.
+        if (TextUtils.isEmpty(email)) {
+            mEmail.setError(getString(R.string.error_field_required));
+            focusView = mEmail;
+            cancel = true;
+        } else if (!isEmailValid(email)) {
+            mEmail.setError(getString(R.string.error_invalid_email));
+            focusView = mEmail;
+            cancel = true;
+        }
+
+        if (cancel) {
+            // There was an error; don't attempt login and focus the first
+            // form field with an error.
+            focusView.requestFocus();
+        } else {
+
+            showProgress(true);
+
+            mAuth.createUserWithEmailAndPassword(email, password)
+                    .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if (task.isSuccessful()) {
+                                // Sign in success, update UI with the signed-in user's information
+                                Log.d(TAG, "createUserWithEmail:success");
+                                FirebaseUser user = mAuth.getCurrentUser();
+
+                                UserProfileChangeRequest profileUpdates = new UserProfileChangeRequest.Builder()
+                                        .setDisplayName(name)
+                                        .build();
+
+                                user.updateProfile(profileUpdates)
+                                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                            @Override
+                                            public void onComplete(@NonNull Task<Void> task) {
+                                                if (task.isSuccessful()) {
+                                                    Log.d(TAG, "User profile updated.");
+                                                }
+                                            }
+                                        });
+
+                                Intent toHome = new Intent(RegisterActivity.this, NavDrawerActivity.class);
+                                startActivity(toHome);
+                                finish();
+                            } else {
+                                // If sign in fails, display a message to the user.
+                                Log.w(TAG, "createUserWithEmail:failure", task.getException());
+                                Toast.makeText(RegisterActivity.this, "Authentication failed.",
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                            // ...
+                        }
+                    });
+        }
+    }
+
+    private boolean isEmailValid(String email) {
+        //TODO: Replace this with your own logic
+        return email.contains("@");
+    }
+
+    private boolean isPasswordValid(String password) {
+        //TODO: Replace this with your own logic
+        return password.length() >= 4;
+    }
+
+    /**
+     * Shows the progress UI and hides the login form.
+     */
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR2)
+    private void showProgress(final boolean show) {
+        // On Honeycomb MR2 we have the ViewPropertyAnimator APIs, which allow
+        // for very easy animations. If available, use these APIs to fade-in
+        // the progress spinner.
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR2) {
+            int shortAnimTime = getResources().getInteger(android.R.integer.config_shortAnimTime);
+
+            mEmail.setVisibility(show ? View.GONE : View.VISIBLE);
+            mEmail.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mEmail.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mPassword.setVisibility(show ? View.GONE : View.VISIBLE);
+            mPassword.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mPassword.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mName.setVisibility(show ? View.GONE : View.VISIBLE);
+            mName.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mName.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mEmail.setVisibility(show ? View.GONE : View.VISIBLE);
+            mEmail.animate().setDuration(shortAnimTime).alpha(
+                    show ? 0 : 1).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mEmail.setVisibility(show ? View.GONE : View.VISIBLE);
+                }
+            });
+
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mProgressView.animate().setDuration(shortAnimTime).alpha(
+                    show ? 1 : 0).setListener(new AnimatorListenerAdapter() {
+                @Override
+                public void onAnimationEnd(Animator animation) {
+                    mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+                }
+            });
+        } else {
+            // The ViewPropertyAnimator APIs are not available, so simply show
+            // and hide the relevant UI components.
+            mProgressView.setVisibility(show ? View.VISIBLE : View.GONE);
+            mEmail.setVisibility(show ? View.GONE : View.VISIBLE);
+            mPassword.setVisibility(show ? View.GONE : View.VISIBLE);
+            mPasswordConfirm.setVisibility(show ? View.GONE : View.VISIBLE);
+            mName.setVisibility(show ? View.GONE : View.VISIBLE);
+        }
     }
 
 }
