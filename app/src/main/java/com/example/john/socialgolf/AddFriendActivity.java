71d6dd690ba17effe7fba9518adbd57422ca426f
jdegrandchamp89 <degranjo@mail.gvsu.edu>
2017-10-19 15:33:10 -0400

    commit for project release 1

--- app/src/main/java/com/example/john/socialgolf/AddFriendActivity.java
+++ app/src/main/java/com/example/john/socialgolf/AddFriendActivity.java
@@ -6,15 +6,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.text.Editable;
 import android.text.TextWatcher;
-import android.util.Log;
 import android.view.View;
 import android.widget.AbsListView;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.example.john.socialgolf.dataObjects.Friends;
